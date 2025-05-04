142f116a6d5238feabc1ff2b1e7309aedac8ff09
adarsh946 <adarsh.shukla2724@gmail.com>
2025-05-04 09:01:09 +0530

    created signup ui

--- components/signupForm.tsx
+++ components/signupForm.tsx
@@ -1,17 +1,36 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 import { createSignupSchema } from "@/schema/signupSchema";
 import { useSignUp } from "@clerk/nextjs";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import z from "zod";
 
 const SignupForm = () => {
+  const router = useRouter();
+
+  const [verificationCode, setVerificationCode] = useState("");
   const [verifying, setIsverifying] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [authError, setAuthError] = useState<string | null>(null);
-  const { signUp, isLoaded } = useSignUp();
+  const [verificationError, setVerificationError] = useState<string | null>(
+    null
+  );
+  const { signUp, isLoaded, setActive } = useSignUp();
 
   const {
     register,
@@ -49,7 +68,77 @@ const SignupForm = () => {
     }
   };
 
-  const handleOnSubmit = async () => {};
+  const handleOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!isLoaded || !signUp) return;
+
+    setIsSubmitting(false);
+    setAuthError(null);
+
+    try {
+      const result = await signUp?.attemptEmailAddressVerification({
+        code: verificationCode,
+      });
+
+      if (result.status === "complete") {
+        await setActive({ session: result.createdSessionId });
+        router.push("/dashboard");
+      } else {
+        console.error("verification incomplete", result);
+        setVerificationError("verification is not able to complete");
+      }
+    } catch (error: any) {
+      console.error("sign- up error", error);
+      setVerificationError(
+        error.errors[0]?.message || "error occured during signup process"
+      );
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  // if(verifying){
+  //   return ()
+  // }
+
+  return (
+    <Card className="max-w-md mx-auto mt-20">
+      <CardHeader>
+        <CardTitle className="text-center text-3xl font-bold">
+          Register
+        </CardTitle>
+        <CardDescription className="text-center text-md">
+          you need to signup to get access in the app
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div>
+            <div className=" flex flex-col py-3">
+              <Label className="text-lg">Email</Label>
+              <Input type="email" placeholder="Enter your Email" />
+            </div>
+            <div className="py-3">
+              <Label className="text-lg">Password</Label>
+              <Input type="text" placeholder="Enter your password" />
+            </div>
+            <div className="py-3">
+              <Label className="text-lg">Confirm Password</Label>
+              <Input type="text" placeholder="confirm your password" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <Button
+          variant={"default"}
+          className="bg-black text-white min-w-[400px] cursor-pointer"
+        >
+          SignUp
+        </Button>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default SignupForm;
